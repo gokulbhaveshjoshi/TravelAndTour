5dfbc78ad1d8f428e3c731132ce0ef58a6041f6d
gokul bhavesh joshi <gokulbhavesh2312@gmail.com>
2023-07-02 17:20:55 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' exact></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
+
     </Router>
 
   );
