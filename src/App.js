0c1f6eb757b894d077c07b4fcea113b37eee9425
gokul bhavesh joshi <gokulbhavesh2312@gmail.com>
2023-07-02 15:05:36 +0530

    button UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hey </h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact></Route>
+      </Routes>
+    </Router>
+
   );
 }
 
